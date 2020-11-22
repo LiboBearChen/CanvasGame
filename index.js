d1a96dd39b5e56aa259e7c45cd6120b0b56ee795
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-22 17:59:34 -0500

    update

--- index.js
+++ index.js
@@ -1,62 +1,69 @@
-const canvas=document.querySelector('canvas')
-const c=canvas.getContext('2d')
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-canvas.width=innerWidth
-canvas.height=innerHeight
+canvas.width = innerWidth
+canvas.height = innerHeight
 
-class Player{
-    constructor(x,y,radius,color){
-        this.x=x
-        this.y=y
-        this.radius=radius
-        this.color=color
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
     }
 
-    draw(){
+    draw() {
         c.beginPath()
-        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false)
-        c.fillStyle=this.color
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
         c.fill()
     }
 }
 
-class Projectile{
-    constructor(x,y,radius,color,velocity){
-        this.x=x
-        this.y=y
-        this.radius=radius
-        this.color=color
-        this.velocity=velocity
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
     }
 
-    draw(){
+    draw() {
         c.beginPath()
-        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false)
-        c.fillStyle=this.color
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
         c.fill()
     }
 
-    update(){
-        this.x=this.x+this.velocity.x
-        this.y=this.y+this.velocity.y
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
     }
 }
 
-const x=canvas.width/2
-const y=canvas.height/2
+const x = canvas.width / 2
+const y = canvas.height / 2
 
-const player=new Player(x,y,30,'blue')
+const player = new Player(x, y, 30, 'blue')
 
 player.draw()
 
-function animate(){
-    requestAnimationFrame(animate)
 
+
+const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {x:1,y:1})
+    
+const projectiles=[projectile]
+
+function animate() {
+    requestAnimationFrame(animate)
+    projectiles.forEach((projectile)=>{projectile.update()})
+    
 }
 
-addEventListener('click',()=>{
-    const projectile=new Projectile(canvas.width/2,canvas.height/2,5,'red',null)
-    projectile.draw()
+addEventListener('click', () => {
+    
 })
 
 animate()
