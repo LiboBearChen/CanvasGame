1bb6588b9b30bc4054026a3a2e3c6b48576b45c9
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-25 13:27:35 -0500

    update

--- index.js
+++ index.js
@@ -104,7 +104,17 @@ function animate() {
     c.clearRect(0,0,canvas.width,canvas.height)
     player.draw()
     projectiles.forEach((projectile)=>{projectile.update()})
-    enemies.forEach((enemy)=>{enemy.update()})
+    enemies.forEach((enemy,index)=>{
+        enemy.update()
+        projectiles.forEach((projectile,projectileIndex)=>{
+            const dist=Math.hypot(projectile.x-enemy.x,projectile.y-enemy.y)
+
+            if(dist-enemy.radius-projectile.radius<1){
+                enemies.splice(index,1)
+                projectiles.splice(projectileIndex,1)
+            }
+        })
+    })
     
 }
 
