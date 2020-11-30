6258e43f3879727ec4b81db90409caa22235e191
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-29 19:36:35 -0500

    update

--- index.js
+++ index.js
@@ -66,68 +66,88 @@ const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue')
-    
-const projectiles=[]
-const enemies=[]
 
-function spawnEnemies(){
-    setInterval(()=>{
-        const radius=Math.random()*26+4
+const projectiles = []
+const enemies = []
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * 26 + 4
         let x
         let y
-        if(Math.random()<0.5){
-            x=Math.random()<0.5? 0-radius:canvas.width+radius
-            y=Math.random()*canvas.height
-        }else{
-            x=Math.random()*canvas.width
-            y=Math.random()<0.5? 0-radius:canvas.height+radius
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
 
-        const color='green'
+        const color = 'green'
 
-        const angle =Math.atan2(
-            canvas.height/2-y,
-            canvas.width/2-x
+        const angle = Math.atan2(
+            canvas.height / 2 - y,
+            canvas.width / 2 - x
         )
-        const velocity={
-            x:Math.cos(angle),
-            y:Math.sin(angle)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
         }
 
-        enemies.push(new Enemy(x,y,radius,color,velocity))
-    },1000)
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+    }, 1000)
 }
 
+let anmationId
 function animate() {
-    requestAnimationFrame(animate)
-    
-    c.clearRect(0,0,canvas.width,canvas.height)
+    anmationId=requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    projectiles.forEach((projectile)=>{projectile.update()})
-    enemies.forEach((enemy,index)=>{
+    projectiles.forEach((projectile,index) => { 
+        projectile.update() 
+        if(projectile.x+projectile.radius<0||
+            projectile.x-projectile.radius>canvas.width||
+            projectile.y+projectile.radius<0||
+            projectile.y-projectile.radius>canvas.height)
+            {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        }
+    })
+    enemies.forEach((enemy, index) => {
         enemy.update()
-        projectiles.forEach((projectile,projectileIndex)=>{
-            const dist=Math.hypot(projectile.x-enemy.x,projectile.y-enemy.y)
 
-            if(dist-enemy.radius-projectile.radius<1){
-                enemies.splice(index,1)
-                projectiles.splice(projectileIndex,1)
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+        if (dist - enemy.radius - player.radius < 1){
+            cancelAnimationFrame(anmationId)
+        }
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+            if (dist - enemy.radius - projectile.radius < 1) {
+                setTimeout(() => {
+                    enemies.splice(index, 1)
+                    projectiles.splice(projectileIndex, 1)
+                }, 0)
+
             }
         })
     })
-    
+
 }
 
 addEventListener('click', (event) => {
-    const angle =Math.atan2(
-        event.clientY-canvas.height/2,
-        event.clientX-canvas.width/2
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
     )
-    const velocity={
-        x:Math.cos(angle),
-        y:Math.sin(angle)
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
     }
-    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)) 
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
 })
 
 animate()
