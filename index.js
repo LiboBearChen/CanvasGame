a40325ea3fe3d41b62437e118091c091cdcebc76
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-11-30 19:58:55 -0500

    update

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ class Enemy {
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 10, 'white')
 
 const projectiles = []
 const enemies = []
@@ -83,7 +83,7 @@ function spawnEnemies() {
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
 
-        const color = 'green'
+        const color = `hsl(${Math.random() * 360},50%,50%)`
 
         const angle = Math.atan2(
             canvas.height / 2 - y,
@@ -100,16 +100,16 @@ function spawnEnemies() {
 
 let anmationId
 function animate() {
-    anmationId=requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    anmationId = requestAnimationFrame(animate)
+    c.fillStyle = 'rgba(0,0,0,0.1)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    projectiles.forEach((projectile,index) => { 
-        projectile.update() 
-        if(projectile.x+projectile.radius<0||
-            projectile.x-projectile.radius>canvas.width||
-            projectile.y+projectile.radius<0||
-            projectile.y-projectile.radius>canvas.height)
-            {
+    projectiles.forEach((projectile, index) => {
+        projectile.update()
+        if (projectile.x + projectile.radius < 0 ||
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y - projectile.radius > canvas.height) {
             setTimeout(() => {
                 projectiles.splice(index, 1)
             }, 0)
@@ -119,7 +119,7 @@ function animate() {
         enemy.update()
 
         const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
-        if (dist - enemy.radius - player.radius < 1){
+        if (dist - enemy.radius - player.radius < 1) {
             cancelAnimationFrame(anmationId)
         }
 
@@ -127,11 +127,14 @@ function animate() {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
             if (dist - enemy.radius - projectile.radius < 1) {
-                setTimeout(() => {
-                    enemies.splice(index, 1)
-                    projectiles.splice(projectileIndex, 1)
-                }, 0)
-
+                if (enemy.radius > 10) {
+                    enemy.radius -= 10
+                } else {
+                    setTimeout(() => {
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0)
+                }
             }
         })
     })
@@ -144,10 +147,10 @@ addEventListener('click', (event) => {
         event.clientX - canvas.width / 2
     )
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
     }
-    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity))
 })
 
 animate()
