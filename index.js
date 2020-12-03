662f20074a3b8bedcd933f64c8c2a5f795c27872
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-12-03 17:07:59 -0500

    update

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+const scoreEl = document.querySelector('#scoreEl')
+
 class Player {
     constructor(x, y, radius, color) {
         this.x = x
@@ -62,7 +64,7 @@ class Enemy {
     }
 }
 
-const friction=0.97
+const friction = 0.97
 class Particle {
     constructor(x, y, radius, color, velocity) {
         this.x = x
@@ -70,11 +72,11 @@ class Particle {
         this.radius = radius
         this.color = color
         this.velocity = velocity
-        this.alpha=1
+        this.alpha = 1
     }
     draw() {
         c.save()
-        c.globalAlpha=this.alpha
+        c.globalAlpha = this.alpha
         c.beginPath()
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
@@ -83,11 +85,11 @@ class Particle {
     }
     update() {
         this.draw()
-        this.velocity.x*=friction
-        this.velocity.y*=friction
+        this.velocity.x *= friction
+        this.velocity.y *= friction
         this.x = this.x + this.velocity.x
         this.y = this.y + this.velocity.y
-        this.alpha-=0.01
+        this.alpha -= 0.01
     }
 }
 
@@ -129,18 +131,19 @@ function spawnEnemies() {
 }
 
 let anmationId
+let score = 0
 function animate() {
     anmationId = requestAnimationFrame(animate)
     c.fillStyle = 'rgba(0,0,0,0.1)'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    particles.forEach((particle,index) => {
-        if(particle.alpha<=0){
-            particles.splice(index,1)
-        }else{
+    particles.forEach((particle, index) => {
+        if (particle.alpha <= 0) {
+            particles.splice(index, 1)
+        } else {
             particle.update()
         }
-        
+
     })
     projectiles.forEach((projectile, index) => {
         projectile.update()
@@ -165,18 +168,24 @@ function animate() {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
             if (dist - enemy.radius - projectile.radius < 1) {
-                for (let i = 0; i < enemy.radius*2; i++) {
-                    particles.push(new Particle(projectile.x,projectile.y,Math.random()*2,enemy.color,{
-                        x:(Math.random()-0.5)*(Math.random()*8),
-                        y:(Math.random()-0.5)*(Math.random()*8)
+
+
+                for (let i = 0; i < enemy.radius * 2; i++) {
+                    particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, {
+                        x: (Math.random() - 0.5) * (Math.random() * 8),
+                        y: (Math.random() - 0.5) * (Math.random() * 8)
                     }))
-                    
+
                 }
-                if (enemy.radius -10> 5) {
-                    gsap.to(enemy,{
-                        radius:enemy.radius-10
+                if (enemy.radius - 10 > 5) {
+                    score += 100
+                    scoreEl.innerHTML = score
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10
                     })
                 } else {
+                    score += 250
+                    scoreEl.innerHTML = score
                     setTimeout(() => {
                         enemies.splice(index, 1)
                         projectiles.splice(projectileIndex, 1)
